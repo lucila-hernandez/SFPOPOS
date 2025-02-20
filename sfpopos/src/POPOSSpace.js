e62def040e9c7aa92fe0ceb519267b4c8f7b8067
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-02-20 09:43:19 -0800

    content component built

--- sfpopos/src/POPOSSpace.js
+++ sfpopos/src/POPOSSpace.js
@@ -2,14 +2,14 @@ function POPOSSpace(props) {
     const { name, image, address } = props
     return (
       <div>
+        <h1>{name}</h1>
+        <div>{address}</div>
         <img
           src={`${process.env.PUBLIC_URL}/images/${image}`}
           width="300"
           height="300"
           alt="Hello"
         />
-        <h1>{name}</h1>
-        <div>{address}</div>
       </div>
     )
   }
