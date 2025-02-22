70a5ab908d29384165c3935540fbd63669153bb9
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-02-21 20:13:40 -0800

    dynamic content and footer

--- sfpopos/src/POPOSSpace.js
+++ sfpopos/src/POPOSSpace.js
@@ -1,18 +1,15 @@
 import './POPOSSpace.css'
 
 function POPOSSpace(props) {
-    const { name, image, address } = props
-    return (
-      <div className="POPOSSpace">
-        <h1>{name}</h1>
-        <div>{address}</div>
-        <img
-          src={`${process.env.PUBLIC_URL}/images/${image}`}
-          width="300"
-          height="300"
-          alt="Hello"
-        />
-      </div>
-    )
-  }
+  const { name, image, address, hours } = props
+  return (
+    <div className="POPOSSpace">
+      <h1>{name}</h1>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+      <div>{address}</div>
+      <div>{hours}</div>
+    </div>
+  )
+}
+
 export default POPOSSpace
